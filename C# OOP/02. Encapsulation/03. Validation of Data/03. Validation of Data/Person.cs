ccdbe6628809b53bdecd8bd599812203734b37b8
vikito11111 <142398365+vikito11111@users.noreply.github.com>
2024-10-15 12:45:14 +0300

    Add files via upload

---
+++ C# OOP/02. Encapsulation/03. Validation of Data/03. Validation of Data/Person.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03._Validation_of_Data
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            private set
+            {
+                if (value != null && value.Length < 3)
+                {
+                    throw new Exception("Invalid length for first name!");
+                }
+
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            private set
+            {
+                if (value != null && value.Length < 3)
+                {
+                    throw new Exception("Invalid length for last name!");
+                }
+
+                lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("Invalid age!");
+                }
+
+                age = value;
+            }
+        }
+
+        public decimal Salary
+        {
+            get
+            {
+                return salary;
+            }
+            set
+            {
+                if (value < 650)
+                {
+                    throw new Exception("Invalid minimum salary! Minimum should be at least 650!");
+                }
+
+                salary = value;
+            }
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age >= 30)
+            {
+                this.Salary += Salary * percentage / 100;
+            }
+            else
+            {
+                this.Salary += Salary * percentage / 200;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives {this.Salary:f2} leva.";
+        }
+    }
+}
---
+++ C# OOP/02. Encapsulation/03. Validation of Data/03. Validation of Data/Program.cs
@@ -0,0 +1,34 @@
+﻿using _03._Validation_of_Data;
+
+var lines = int.Parse(Console.ReadLine());
+
+var people = new List<Person>();
+
+for (int i = 0; i < lines; i++)
+{
+    string[] personInfo = Console.ReadLine()
+        .Split(' ');
+
+	try
+	{
+        Person person = new Person(personInfo[0], personInfo[1], int.Parse(personInfo[2]), decimal.Parse(personInfo[3]));
+
+        people.Add(person);
+    }
+	catch (Exception ex)
+	{
+        Console.WriteLine(ex.Message);
+    }
+}
+
+decimal percentage = decimal.Parse(Console.ReadLine());
+
+foreach (var person in people)
+{
+    person.IncreaseSalary(percentage);
+}
+
+foreach (Person person in people.OrderBy(x => x.Salary).ThenBy(x => x.Age))
+{
+    Console.WriteLine(person.ToString());
+}
