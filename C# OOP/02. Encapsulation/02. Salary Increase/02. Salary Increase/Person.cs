1615054350df6b3322ba6e17e3e62b5ef7860935
vikito11111 <142398365+vikito11111@users.noreply.github.com>
2024-10-15 12:20:05 +0300

    Add files via upload

---
+++ C# OOP/02. Encapsulation/02. Salary Increase/02. Salary Increase/Person.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02._Salary_Increase
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            private set
+            {
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            private set
+            {
+                lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            private set
+            {
+                age = value;
+            }
+        }
+
+        public decimal Salary
+        {
+            get 
+            { 
+                return salary; 
+            }
+            set 
+            { 
+                salary = value; 
+            }
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age >= 30)
+            {
+                this.Salary += Salary * percentage / 100;
+            }
+            else
+            {
+                this.Salary += Salary * percentage / 200;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives {this.Salary:f2} leva.";
+        }
+    }
+}
---
+++ C# OOP/02. Encapsulation/02. Salary Increase/02. Salary Increase/Program.cs
@@ -0,0 +1,27 @@
+﻿using _02._Salary_Increase;
+
+var lines = int.Parse(Console.ReadLine());
+
+var people = new List<Person>();
+
+for (int i = 0; i < lines; i++)
+{
+    string[] personInfo = Console.ReadLine()
+        .Split(' ');
+
+    Person person = new Person(personInfo[0], personInfo[1], int.Parse(personInfo[2]), decimal.Parse(personInfo[3]));
+
+    people.Add(person);
+}
+
+decimal percentage = decimal.Parse(Console.ReadLine());
+
+foreach (var person in people)
+{
+    person.IncreaseSalary(percentage);
+}
+
+foreach (Person person in people.OrderBy(x => x.Salary).ThenBy(x => x.Age))
+{
+    Console.WriteLine(person.ToString());
+}
