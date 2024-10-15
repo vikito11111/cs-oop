f484f3806409a410e3dc17df341215aa6b8252e0
vikito11111 <142398365+vikito11111@users.noreply.github.com>
2024-10-15 13:22:05 +0300

    Add files via upload

---
+++ C# OOP/02. Encapsulation/04. Team/04. Team/Program.cs
@@ -0,0 +1,7 @@
+﻿using _04._Team;
+
+Person person = new Person("Ivan", "Rainov", 24, 1000);
+
+Team team = new Team();
+
+team.Add(person);
---
+++ C# OOP/02. Encapsulation/04. Team/04. Team/Team.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04._Team
+{
+    public class Team
+    {
+        private List<Person> players;
+
+        public Team()
+        {
+            players = new List<Person>();
+        }
+
+        public IReadOnlyCollection<Person> Players { get; set; }
+
+        public void Add(Person person)
+        {
+            if (person == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            this.players.Add(person);
+        }
+    }
+}
