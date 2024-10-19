b2a4090432a9835acf24be1b70895db82de9445f
vikito11111 <142398365+vikito11111@users.noreply.github.com>
2024-10-19 16:48:50 +0300

    Add files via upload

---
+++ C# OOP/04. Polymorphism/01. Polymorphism Demo/01. Polymorphism Demo/Program.cs
@@ -0,0 +1,32 @@
+﻿List<Animal> zoo = new List<Animal>();
+
+zoo.Add(new Dog());
+zoo.Add(new Bird());
+zoo.Add(new Cat());
+zoo.Add(new Snake());
+
+
+public class Animal
+{
+    public string Name { get; set; }
+}
+
+public class Cat : Animal
+{
+
+}
+
+public class Dog : Animal
+{
+
+}
+
+public class Bird : Animal
+{
+
+}
+
+public class Snake : Animal
+{
+
+}
