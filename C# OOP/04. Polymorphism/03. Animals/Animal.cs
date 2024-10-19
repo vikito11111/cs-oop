728ea480eebf25b830ab512f899860527fed2e1f
vikito11111 <142398365+vikito11111@users.noreply.github.com>
2024-10-19 17:53:58 +0300

    Add files via upload

---
+++ C# OOP/04. Polymorphism/03. Animals/Animal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Animals
+{
+    public class Animal
+    {
+        public Animal(string name, string favouriteFood)
+        {
+            Name = name;
+
+            FavouriteFood = favouriteFood;
+        }
+
+        public string Name { get; private set; }
+
+        public string FavouriteFood { get; private set; }
+
+        public virtual string ExplainSelf()
+        {
+            return string.Format($"I am {Name} and my favourite food is {FavouriteFood}");
+        }
+    }
+}
---
+++ C# OOP/04. Polymorphism/03. Animals/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Animals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal cat = new Cat("Pesho", "Whiskas");
+
+            Animal dog = new Dog("Gosho", "Meat");
+
+            Console.WriteLine(cat.ExplainSelf());
+
+            Console.WriteLine(dog.ExplainSelf());
+
+            Console.ReadLine();
+        }
+    }
+}
