302bfd1da3a6c791dfcd0a5a6de894a7a850cabb
vikito11111 <142398365+vikito11111@users.noreply.github.com>
2024-10-24 16:19:36 +0300

    Add files via upload

---
+++ C# OOP/05. Exception Handling/03. Square Root/03. Square Root/Program.cs
@@ -0,0 +1,19 @@
+﻿try
+{
+	int number = int.Parse(Console.ReadLine());
+
+	if (number < 0)
+	{
+		throw new ArgumentOutOfRangeException(nameof(number), "Invalid number!");
+	}
+
+    Console.WriteLine(Math.Sqrt((number)));
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    Console.WriteLine("Goodbye");
+}
