4658e912f5bd0bf690ab5143b4aa4ff97e97e3f2
vikito11111 <142398365+vikito11111@users.noreply.github.com>
2024-10-24 15:55:40 +0300

    Add files via upload

---
+++ C# OOP/05. Exception Handling/02. Exception Hierarchy/02. Exception Hierarchy/Program.cs
@@ -0,0 +1,24 @@
+﻿string str = Console.ReadLine();
+
+try
+{
+	Int32.Parse(str);
+
+    Console.WriteLine("Successful Parsing! " + str);
+}
+catch (FormatException ex)
+{
+    Console.WriteLine("Hello from FormatException. " + ex.Message);
+}
+catch (OverflowException ex)
+{
+    Console.WriteLine("Hello from OverflowException. " + ex.Message);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Hello from Exception. " + ex.Message);
+}
+finally
+{
+    Console.WriteLine("End.");
+}
