fde73ad7994058529d1a7cd865141a7f16912de4
vikito11111 <142398365+vikito11111@users.noreply.github.com>
2024-10-24 15:43:23 +0300

    Add files via upload

---
+++ C# OOP/05. Exception Handling/01. Exception Handling/01. Exception Handling/Program.cs
@@ -0,0 +1,14 @@
+﻿try
+{
+    int number = int.Parse(Console.ReadLine());
+
+    Console.WriteLine(number);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    Console.WriteLine("Finally!");
+}
