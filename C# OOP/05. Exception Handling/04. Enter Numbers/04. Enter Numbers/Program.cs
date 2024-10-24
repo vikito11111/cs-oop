31e57623482dff5c98a0ccca7c4f19a4772a2b5a
vikito11111 <142398365+vikito11111@users.noreply.github.com>
2024-10-24 16:40:23 +0300

    Add files via upload

---
+++ C# OOP/05. Exception Handling/04. Enter Numbers/04. Enter Numbers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Channels;
+
+List<int> numbers = new List<int>();
+
+while (numbers.Count != 10)
+{
+    try
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        if (number <= 1 || number >= 100)
+        {
+            int lastNumber = numbers.LastOrDefault() == 0 
+                ? 1 
+                : numbers.LastOrDefault();
+
+            throw new ArgumentOutOfRangeException(nameof(number), $"Your number is not in range of ({lastNumber} - 100)!");
+        }
+        else if (numbers.Count == 0)
+        {
+            numbers.Add(number);
+        }
+        else if (numbers.LastOrDefault() < number)
+        {
+            numbers.Add(number);
+        }
+        else
+        {
+            throw new ArgumentOutOfRangeException(nameof(number), $"Your number is not in range of ({numbers.LastOrDefault()} - 100)!");
+        }
+    }
+    catch (FormatException ex)
+	{
+        Console.WriteLine("Invalid number!");
+    }
+    catch (ArgumentOutOfRangeException ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
+
+Console.WriteLine(string.Join(", ", numbers));
