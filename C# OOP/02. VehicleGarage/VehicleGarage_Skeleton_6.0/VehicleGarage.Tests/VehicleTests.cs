d23fda9f8b607f7aa013e2dacc896c5e1abd78db
vikito11111 <142398365+vikito11111@users.noreply.github.com>
2024-10-25 14:10:05 +0300

    Add files via upload

---
+++ C# OOP/02. VehicleGarage/VehicleGarage_Skeleton_6.0/VehicleGarage.Tests/VehicleTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace VehicleGarage.Tests
+{
+    public class VehicleTests
+    {
+        [TestCase("VW", "Golf", "CB7777XA")]
+        [TestCase("Toyota", "Rav4", "PB0000XA")]
+        public void ConstructorShouldInitilizeAllValues(string make, string model, string licensePlate)
+        {
+            //Arrange
+            Vehicle vehicle = new Vehicle(make, model, licensePlate);
+
+            //Assert
+            Assert.AreEqual(vehicle.Brand, make);
+            Assert.AreEqual(vehicle.Model, model);
+            Assert.AreEqual(vehicle.LicensePlateNumber, licensePlate);
+            Assert.AreEqual(vehicle.IsDamaged, false);
+            Assert.AreEqual(vehicle.BatteryLevel, 100);
+        }
+    }
+}
