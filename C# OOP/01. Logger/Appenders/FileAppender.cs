5cc624affddf8be4ae7436d70f9802e92f84bc38
vikito11111 <142398365+vikito11111@users.noreply.github.com>
2024-10-29 15:13:16 +0200

    Add files via upload

---
+++ C# OOP/01. Logger/Appenders/FileAppender.cs
@@ -0,0 +1,29 @@
+﻿using _01.Logger.Enums;
+using _01.Logger.Loggers;
+using _01.LoggerTask.Appenders;
+using _01.LoggerTask.Layouts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Logger.Appenders
+{
+    public class FileAppender : IAppender
+    {
+        private ILogFile logFile;
+
+        public FileAppender(ILayout layout, ILogFile logFile)
+        {
+            Layout = layout;
+        }
+
+        public ILayout Layout { get; }
+
+        public void Append(string dateTime, LogLevel logLevel, string message)
+        {
+            this.logFile.Write(string.Format(Layout.Format, dateTime, logLevel, message));
+        }
+    }
+}
---
+++ C# OOP/01. Logger/Loggers/ILogger.cs
@@ -0,0 +1,18 @@
+﻿using _01.LoggerTask.Appenders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.LoggerTask.Loggers
+{
+    public interface ILogger
+    {
+        IAppender Appender { get; }
+
+        void Error(string dateTime, string message);
+
+        void Info(string dateTime, string message);
+    }
+}
