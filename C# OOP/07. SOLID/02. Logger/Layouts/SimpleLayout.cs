0cb492fe5b788258f0f75c055d9a4fac10fcefbf
vikito11111 <142398365+vikito11111@users.noreply.github.com>
2024-10-29 15:14:10 +0200

    Add files via upload

---
+++ C# OOP/07. SOLID/02. Logger/Layouts/SimpleLayout.cs
@@ -0,0 +1,14 @@
+﻿using _01.LoggerTask.Layouts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.LoggerTask
+{
+    public class SimpleLayout : ILayout
+    {
+        public string Format => "{0} - {1} - {2}";
+    }
+}
---
+++ C# OOP/07. SOLID/02. Logger/Loggers/ILogFile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Logger.Loggers
+{
+    public interface ILogFile
+    {
+        void Write(string message);
+
+        int Size { get; }
+    }
+}
