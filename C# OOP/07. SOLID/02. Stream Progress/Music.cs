fa7f8f3346ecfa60dd10408740c19727f92b5e61
vikito11111 <142398365+vikito11111@users.noreply.github.com>
2024-10-31 15:06:48 +0200

    Add files via upload

---
+++ C# OOP/07. SOLID/02. Stream Progress/Music.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01.Stream_Progress
+{
+    public class Music : File
+    {
+        public Music(string artist, string album, int length, int bytesSent)
+            : base(artist, length, bytesSent)
+        {
+            this.Artist = artist;
+            this.Album = album;
+        }
+
+        public string Artist { get; }
+
+        public string Album { get; }
+    }
+}
---
+++ C# OOP/07. SOLID/02. Stream Progress/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace P01.Stream_Progress
+{
+    public class Program
+    {
+        static void Main()
+        {
+            File music = new Music("Pesho", "Pesho's songs", 10, 10);
+
+            File file = new File("File", 20, 30);
+
+            StreamProgressInfo streamProgressInfo1 = new StreamProgressInfo(music);
+            StreamProgressInfo streamProgressInfo2 = new StreamProgressInfo(file);
+
+            streamProgressInfo1.CalculateCurrentPercent();
+            streamProgressInfo2.CalculateCurrentPercent();
+        }
+    }
+}
