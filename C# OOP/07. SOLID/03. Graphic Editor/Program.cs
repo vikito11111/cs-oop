925a4e7a2c70cca8b91c28425af8a5b1f954f426
vikito11111 <142398365+vikito11111@users.noreply.github.com>
2024-10-31 15:13:03 +0200

    Add files via upload

---
+++ C# OOP/07. SOLID/03. Graphic Editor/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace P02.Graphic_Editor
+{
+    class Program
+    {
+        static void Main()
+        {
+            GraphicEditor editor = new GraphicEditor();
+
+            editor.DrawShape(new Circle());
+            editor.DrawShape(new Square());
+            editor.DrawShape(new Rectangle());
+        }
+    }
+}
