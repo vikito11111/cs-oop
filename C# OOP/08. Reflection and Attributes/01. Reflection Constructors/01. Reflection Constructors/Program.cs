299db7984d097fc7bc62f6bd918c4927cea4bc14
vikito11111 <142398365+vikito11111@users.noreply.github.com>
2024-11-01 17:59:31 +0200

    Add files via upload

---
+++ C# OOP/08. Reflection and Attributes/01. Reflection Constructors/01. Reflection Constructors/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+
+Type type = typeof(Human);
+
+ConstructorInfo[] constructorInfos = type.GetConstructors();
+
+Console.WriteLine($"Count: {constructorInfos.Length}");
+
+ConstructorInfo constructorInfo = constructorInfos[0];
+
+Console.WriteLine(constructorInfo.GetParameters().Length);
+
+
+ParameterInfo[] parameters = constructorInfo.GetParameters();
+Console.WriteLine($"Constructor: {constructorInfo.Name}");
+Console.WriteLine(parameters.Length);
+
+foreach (var parameter in parameters)
+{
+    Console.WriteLine($"{parameter.ParameterType.Name} - {parameter.Name}");
+}
+
+class Human
+{
+    private string name = "The Human";
+
+    public Human()
+    {
+
+    }
+
+    public Human(string name)
+    {
+        this.name = name;
+    }
+}
