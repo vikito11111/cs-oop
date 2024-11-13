31ba8d3f8214e0c775af17442c91b7b96282be0d
vikito11111 <142398365+vikito11111@users.noreply.github.com>
2024-11-13 18:34:42 +0200

    Add files via upload

---
+++ C# OOP/09. Design Patterns/03. DrawIOCompositePattern/03. DrawIOCompositePattern/Position.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03._DrawIOCompositePattern
+{
+    public class Position
+    {
+        public Position(int row, int col)
+        {
+            Row = row;
+            Col = col;
+        }
+
+        public int Row { get; set; }
+
+        public int Col { get; set; }
+    }
+}
---
+++ C# OOP/09. Design Patterns/03. DrawIOCompositePattern/03. DrawIOCompositePattern/Program.cs
@@ -0,0 +1,29 @@
+﻿using _03._DrawIOCompositePattern;
+using _03._DrawIOCompositePattern.Shapes;
+
+Shape page = new Shape(new Position(0, 0));
+
+Shape leftAd = new Shape(new Position(0, 0));
+Rectangle leftRectangle = new Rectangle(new Position(10, 5), 20, 5);
+Line leftLine = new Line(new Position(15, 5), 20);
+Text leftText = new Text(new Position(17, 5), "The best offer!");
+Line leftSecondLine = new Line(new Position(19, 5), 20);
+
+leftRectangle.AddChild(leftLine);
+leftRectangle.AddChild(leftText);
+leftRectangle.AddChild(leftSecondLine);
+
+leftAd.AddChild(leftRectangle);
+
+leftAd.Draw();
+
+for (int i = 0; i < 10; i++)
+{
+    Thread.Sleep(100);
+
+    leftAd.Move(Direction.Right);
+
+    Console.Clear();
+
+    leftAd.Draw();
+}
