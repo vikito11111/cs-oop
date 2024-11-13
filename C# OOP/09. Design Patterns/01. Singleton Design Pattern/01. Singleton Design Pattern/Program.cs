4a5e4eb26209e9d1c13cedff045d612a4c0e8d43
vikito11111 <142398365+vikito11111@users.noreply.github.com>
2024-11-13 15:28:53 +0200

    Add files via upload

---
+++ C# OOP/09. Design Patterns/01. Singleton Design Pattern/01. Singleton Design Pattern/Program.cs
@@ -0,0 +1,18 @@
+﻿using _01._Singleton_Design_Pattern;
+
+for (int i = 0; i < 10000; i++)
+{
+    new Thread(() =>
+    {
+        var test = UserSingleton.Instance.Name;
+    }).Start();
+}
+
+Console.WriteLine("Before initializing singleton");
+
+UserSingleton loggedUser = UserSingleton.Instance;
+
+loggedUser.Name = "Pesho";
+loggedUser.Password = "12345";
+
+loggedUser.ChangePassword("pesho12345");
